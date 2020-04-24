123a29e4fe0118c27c9fe6aae5717a896ea6c92b
kartikeyajaiswal <47529059+kartikeyajaiswal@users.noreply.github.com>
2020-04-24 23:18:10 +0530

    Update ee18btech11025.py

--- ketan_control/codes/ee18btech11025.py
+++ ketan_control/codes/ee18btech11025.py
@@ -2,6 +2,10 @@ import control
 import matplotlib.pyplot as plt
 import numpy
 
+#if using termux
+import subprocess
+import shlex
+
 num = [100,500]  #numerator of transfer function
 den = [1,3,4,12,0]  #denominator of transfer function
 
@@ -11,9 +15,14 @@ w = numpy.logspace(-3,3,5000)
 print(G)
 
 control.nyquist(G,w); # nyquist plot
-plt.savefig('nyquist')
-pz = control.pzmap(G); #pole-zero plot
-print(pz)
-#plt.xlim(-100,100)
-#plt.ylim(-100,100)
-plt.savefig('polezero')
+plt.savefig('./fig/ee18btech11025/nyquist.pdf')
+plt.savefig('./fig/ee18btech11025/nyquist.eps') #if using termux
+subprocess.run(shlex.split("termux-open ./figs/ee18btech11025/nyquist.pdf"))
+
+p, z = control.pzmap(G); #pole-zero plot
+print('poles:', p)
+print('zeros:', z)
+plt.savefig('./figure/ee18bteh11025/polezero.pdf')
+plt.savefig('./figure/ee18bteh11025/polezero.eps') #if using termux
+
+subprocess.run(shlex.split("termux-open ./figs/ee18btech11025/polezero.pdf")) #if using termux
